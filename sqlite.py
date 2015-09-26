53db487bc29df6706d9f8df984e58aedbb186b5b
Lucas <lucasribeiroabreu@gmail.com>
2015-09-26 11:27:01 -0300

    sqlite+matplotlib graphs
    alternate style of matplotlib

--- sqlite.py
+++ sqlite.py
@@ -1,3 +1,4 @@
+from string import split
 from pylab import rcParams
 import sqlite3
 import datetime
@@ -7,7 +8,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 pd.options.display.mpl_style = 'default'
 import numpy as np
-conn = sqlite3.connect('/Users/Lucas/Downloads/Banco0.db')
+conn = sqlite3.connect('/Users/Lucas/Downloads/Banco-5.db')
 c = conn.cursor()
 
 def tableCreate():
@@ -81,11 +82,6 @@ def separaHeD(df):
     data = pd.Series(dataList)
     hora = pd.Series(horaList)
 
-    print "Tamanho dos dataframes"
-    print data.size
-    print hora.size
-    print df2["ID"].size
-
     df2 = pd.concat([df2,data,hora],axis=1)
 
     #               0      1      2       3      4      5
@@ -105,25 +101,45 @@ def readProducts():
 
 
 def graphs(df):
+
+    f = open('/Users/Lucas/Desktop/quinta/dados.txt', 'w')
+
     plt.rc("font", size=8)
-    print "Total Diario"
-    print df.iloc[:,(3,4)].groupby("DATA").sum()
+    s= "Total Diario"
+    f.write(str(s))
+    s= df.iloc[:,(3,4)].groupby("DATA").sum()
+    f.write(str(s))
+    s="\n"
+    f.write(str(s))
+
     df.iloc[:,(3,4)].groupby("DATA").sum().plot()
     plt.ylabel("R$")
     plt.title("Total Diario")
     plt.savefig("/Users/Lucas/Desktop/quinta/TotalDiario.pdf")
     plt.show()
 
-    print "Total por conta"
-    print df.iloc[:,(1,3)].groupby("CONTA").sum().plot()
+    s= "Total por conta"
+
+    df.iloc[:,(1,3)].groupby("CONTA").sum().plot()
+    s=df.iloc[:,(1,3)].groupby("CONTA").sum()
+
+
+    df.iloc[:,(1,3)].groupby("CONTA").sum()
     plt.ylabel("R$")
     plt.title("Total por conta paga")
     plt.savefig("/Users/Lucas/Desktop/quinta/TotalporConta.pdf")
 
     plt.show()
-
-    print "Total de produto por dia:"
-    print df.iloc[:,(2,3,4)].groupby(["NOME","DATA"]).sum()
+    s="\n"
+    f.write(str(s))
+    s="\n"
+    f.write(str(s))
+    s= "Total de produto por dia:"
+    f.write(str(s))
+    s=df.iloc[:,(2,3,4)].groupby(["NOME","DATA"]).sum()
+    f.write(str(s))
+    s="\n"
+    f.write(str(s))
 
     print "Total de Vendas de cada produto ate hj:"
     print df.iloc[:,(2,3,4)].groupby(["NOME"]).sum()
@@ -136,20 +152,70 @@ def graphs(df):
     df[df["DATA"]>"10/09/2015"].iloc[:,(2,3,4)].groupby(["NOME"]).sum().plot(kind="bar")
     plt.ylabel("R$")
     plt.title("Total de vendas")
-    plt.subplots_adjust(bottom=0.28)
+    plt.subplots_adjust(bottom=0.45)
     plt.savefig("/Users/Lucas/Desktop/quinta/Total.pdf")
     plt.show()
-
-    df[df["DATA"]>"10/09/2015"].iloc[:,(2,3,4)].groupby(["NOME"]).sum().plot(kind="pie", subplots=True)
-    plt.ylabel("R$")
-    plt.title("Total de vendas")
-    plt.savefig("/Users/Lucas/Desktop/quinta/Total2.pdf")
-    plt.show()
+    f.close()
     return df
 
 
+def juntaCategoria(df):
+    dfCat = getCategoriasDF()
+    dfConta = getContasDF()
+    df = pd.merge(dfConta,dfCat,on=['CODIGO','CODIGO'])
+
+    s= df["CATEGORIA"].str.split("*",3)
+
+    print "------"
+    df = df.set_index("ID")
+    category = []
+    for i, row in enumerate(s):
+        category.append(row[2])
+
+    df.CATEGORIA = category
+    return df
+
+def graphCat(df):
+
+    df.iloc[:,(6,7)].groupby("CATEGORIA").sum().plot(kind="bar")
+
+    plt.subplots_adjust(bottom=0.32)
+    plt.savefig("/Users/Lucas/Desktop/quinta/Categoria.pdf")
+    plt.show()
+    return df
+
+def getCategorias(df):
+    sql = "SELECT * FROM CATEGORIAS"
+    result = c.execute(sql)
+    for i, row in enumerate(result):
+            #print i
+        print str(row)
+
+def getCategoriasDF():
+    df = pd.read_sql_query("SELECT * from CATEGORIAS", conn)
+    #print df
+    return df
+
+def getContasDF():
+    df = pd.read_sql_query("SELECT * from CONTAS", conn)
+    #print df
+    return df
+def getProdutosDF():
+    df = pd.read_sql_query("SELECT * from PRODUTOS", conn)
+    #print df
+    return df
+
 df = readToDf()
 df = separaHeD(df)
+df.to_csv("/Users/Lucas/Desktop/quinta/dfdatahora.csv",encoding='utf-8')
+print df
 print "---------------"
-graphs(df)
+df = graphs(df)
+
+df = juntaCategoria(df)
+
+df = graphCat(df)
+
+df.to_csv("/Users/Lucas/Desktop/quinta/df.csv",encoding='utf-8')
+
 
